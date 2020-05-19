a50c1e72f331ed882347fe7165c2915ca0faea9b
Mipo Technologies Ltd <mipotech@users.noreply.github.com>
2020-05-19 16:01:13 +0300

    Relaxed allowed_actions validation rules

--- src/models/Permission.php
+++ src/models/Permission.php
@@ -86,7 +86,7 @@ class Permission extends ActiveRecord implements CmsRecordInterface
             [['entity_type', 'entity_name', 'allowed_actions', 'scope',], 'required'],
             ['entity_type', 'in', 'range' => array_keys(PermissionEntityTypes::getList())],
             [['entity_name'], 'string'],
-            [['allowed_actions', 'conditions'], 'each', 'rule' => ['in', 'range' => array_keys(PermissionActions::getList())]],
+            [['allowed_actions', 'conditions'], 'each', 'rule' => ['string']],
             ['scope', 'in', 'range' => array_keys(PermissionScopes::getList())],
             ['scope_id', 'required', 'when' => function($model) {
                 return in_array($model->scope, [
