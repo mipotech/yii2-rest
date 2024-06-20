c2058f739dbe8b4c2f7c27d5e1d390d13250c8bc
EliMipo <eli@mipo.co.il>
2024-06-20 13:06:46 +0300

    init

--- src/controllers/BaseCrudController.php
+++ src/controllers/BaseCrudController.php
@@ -22,6 +22,11 @@ abstract class BaseCrudController extends ActiveController
     public $searchModelClass = null;
 
     /**
+     * @var boolean manual not found error response
+     */
+    public $manualNotFoundError = false;
+
+    /**
      * @inheritdoc
      */
     protected function generatePermissionsQuery(string $action, $model, array $params)
@@ -212,7 +217,9 @@ abstract class BaseCrudController extends ActiveController
         if (isset($model)) {
             return $model;
         } else {
-            throw new \yii\web\NotFoundHttpException();
+            if(!$this->manualNotFoundError) {
+                throw new \yii\web\NotFoundHttpException();
+            }
         }
     }
 
